8a276996de6aad10a7f63f1648769d587ff1e725
Jason Huang <jason97521@gmail.com>
2023-03-29 08:18:25 -0400

    create forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react"
 import "./app.css"
+import FormInput from "./components/FormInput"
 
 const App = () => {
+  const [username, setUsername] = useState("")
+
+  console.log(username)
   return (
-    <div>
-      Hello World!
+    <div className="app">
+      <form>
+        <FormInput placeholder="Username" setUsername={setUsername}/>
+        <FormInput placeholder="Email"/>
+        <FormInput placeholder="Full Name"/>
+        <FormInput placeholder="asd asd"/>
+      </form>
     </div>
   )
 }
