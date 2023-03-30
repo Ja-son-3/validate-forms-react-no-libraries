9c66506328b438358a29bbc06aa8778a0430aea6
Jason Huang <jason97521@gmail.com>
2023-03-30 02:28:47 -0400

    pattern to validate inputs

--- src/App.jsx
+++ src/App.jsx
@@ -16,36 +16,47 @@ const App = () => {
       id:1,
       name: "username",
       type: "text",
-      placeholde: "Username",
-      label: "Username"
+      placeholder: "Username",
+      errorMessage: "Username should be 3-16 characters and shouldn't include any special characters",
+      label: "Username",
+      pattern:"^[A-Za-z0-9]{3,16}$",
+      required: true,
     },
     {
       id:2,
       name: "email",
-      type: "text",
-      placeholde: "Email",
-      label: "Email"
+      type: "email",
+      placeholder: "Email",
+      errorMessage: "It should be a valid email address",
+      label: "Email",
+      required: true,
     },
     {
       id:3,
       name: "birthday",
-      type: "text",
-      placeholde: "Birthday",
-      label: "Birthday"
+      type: "date",
+      placeholder: "Birthday",
+      label: "Birthday",
     },
     {
       id:4,
       name: "password",
       type: "password",
-      placeholde: "Password",
-      label: "Password"
+      placeholder: "Password",
+      errorMessage: "Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character",
+      label: "Password",
+      pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
+      required: true,
     },
     {
       id:5,
       name: "confirmPassword",
       type: "password",
-      placeholde: "Confirm Password",
-      label: "Confirm Password"
+      placeholder: "Confirm Password",
+      errorMessage: "Passwords don't match",
+      label: "Confirm Password",
+      pattern: values.password,
+      required: true,
     },
   ]
 
@@ -61,6 +72,7 @@ const App = () => {
   return (
     <div className="app">
       <form onSubmit={handleSubmit}>
+        <h1>Register</h1>
         {inputs.map(input => (
           <FormInput key={input.id} {...input} value={values[input.name]} onChange={onChange}/>
         ))}
